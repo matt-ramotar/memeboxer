fa99e852ceb45d2f703ddc8b82c44ada42543d19
Matt Ramotar <mramotar@dropbox.com>
2021-12-19 13:04:51 -0500

    Add template grid to new meme flow

--- src/layout/main/NewMemeFlow.tsx
+++ src/layout/main/NewMemeFlow.tsx
@@ -1,7 +1,12 @@
-import { Box, Modal, Typography, useTheme } from "@material-ui/core";
+import { Box, Modal, useTheme } from "@material-ui/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { BeatLoader } from "react-spinners";
 import { RootState } from "../../store";
 import { Page, setActivePage, toggleCreateTemplate } from "../../store/view";
+
+const rootUrl = "http://localhost:5000";
 
 export default function NewMemeFlow(): JSX.Element {
   const dispatch = useDispatch();
@@ -20,9 +25,63 @@ export default function NewMemeFlow(): JSX.Element {
       style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}
       BackdropProps={{ style: { backgroundColor: "rgba(0,0,0,0.85)" } }}
     >
-      <Box style={{ backgroundColor: theme.palette.background.paper, width: 900, height: 900 }}>
-        <Typography>Modal</Typography>
+      <Box style={{ backgroundColor: theme.palette.background.paper, width: 600, height: 600, overflowY: "scroll" }}>
+        <TemplateGrid />
       </Box>
     </Modal>
+  );
+}
+
+export function TemplateGrid(): JSX.Element {
+  const [templates, setTemplates] = useState<any[] | null>(null);
+
+  const userId = useSelector((state: RootState) => state.user.id);
+
+  useEffect(() => {
+    async function fetchTemplates() {
+      const responseFromServer = await axios.get(`${rootUrl}/v1/templates`);
+      console.log(responseFromServer);
+      setTemplates([...responseFromServer.data]);
+    }
+
+    fetchTemplates();
+  }, []);
+
+  return (
+    <Box style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+      {templates?.map((template) => (
+        <Template key={template._id} id={template._id} />
+      ))}
+    </Box>
+  );
+}
+
+interface TemplateProps {
+  id: string;
+}
+
+function Template(props: TemplateProps): JSX.Element {
+  const [signedUrl, setSignedUrl] = useState<string | null>(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function fetchSignedUrl() {
+      const response = await axios.get(`${rootUrl}/storage/${props.id}`);
+      console.log("response", response);
+      setSignedUrl(response.data.data);
+    }
+
+    fetchSignedUrl();
+  }, []);
+
+  return (
+    <div>
+      {(!isLoaded || !signedUrl) && (
+        <Box style={{ height: 150, width: 150, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+          <BeatLoader color="#2F7EFE" loading={!isLoaded} />
+        </Box>
+      )}
+      <img src={signedUrl!} alt="null" style={{ height: 150, width: 150, objectFit: "cover", display: isLoaded ? "inline" : "none" }} onLoad={() => setIsLoaded(true)} />
+    </div>
   );
 }
--- src/pages/App/CreateTemplate.tsx
+++ src/pages/App/CreateTemplate.tsx
@@ -1,15 +1,27 @@
-import { TextField, Typography } from "@material-ui/core";
+import { Box, TextField, Typography } from "@material-ui/core";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { useSelector } from "react-redux";
 import UploadFileIcon from "../../assets/icons/UploadFileIcon";
 import { RootState } from "../../store";
+const rootUrl = "http://localhost:5000";
 
 export default function CreateTemplate(): JSX.Element {
   const [name, setName] = useState<string | null>(null);
+  const [templates, setTemplates] = useState<any[] | null>(null);
 
   const userId = useSelector((state: RootState) => state.user.id);
+
+  useEffect(() => {
+    async function fetchTemplates() {
+      const responseFromServer = await axios.get(`${rootUrl}/v1/templates`);
+      console.log(responseFromServer);
+      setTemplates([...responseFromServer.data]);
+    }
+
+    fetchTemplates();
+  }, []);
 
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
     accept: "image/jpeg, image/png",
@@ -17,7 +29,7 @@ export default function CreateTemplate(): JSX.Element {
   });
 
   async function uploadFile(files: any[]) {
-    const responseFromServer = await axios.post("http://localhost:5000/v1/templates", { name, userId });
+    const responseFromServer = await axios.post(`${rootUrl}/v1/templates`, { name, userId });
     const { id } = responseFromServer.data;
 
     const reader = new FileReader();
@@ -37,7 +49,7 @@ export default function CreateTemplate(): JSX.Element {
       const entityTag = (await response.data.data.ETag) as string;
       const cleanEntityTag = entityTag.replaceAll(/['"]+/g, "");
 
-      await axios.post(`http://localhost:5000/v1/templates/${await id}/tag`, { entityTag: cleanEntityTag });
+      await axios.post(`${rootUrl}/v1/templates/${await id}/tag`, { entityTag: cleanEntityTag });
     };
 
     reader.readAsDataURL(files[0]);
@@ -56,6 +68,35 @@ export default function CreateTemplate(): JSX.Element {
           Drag image here
         </Typography>
       </div>
+
+      <Box>
+        {templates?.map((template) => (
+          <Template key={template._id} id={template._id} />
+        ))}
+      </Box>
     </div>
   );
+}
+
+interface TemplateProps {
+  id: string;
+}
+
+function Template(props: TemplateProps): JSX.Element {
+  const [signedUrl, setSignedUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchSignedUrl() {
+      const response = await axios.get(`${rootUrl}/storage/${props.id}`);
+      console.log("response", response);
+      setSignedUrl(response.data.data);
+    }
+
+    fetchSignedUrl();
+  }, []);
+
+  if (signedUrl) {
+    return <img src={signedUrl} alt="null" style={{ height: 200, width: 200, objectFit: "cover" }} />;
+  }
+  return <Typography>Loading</Typography>;
 }
