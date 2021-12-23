684d5e3ad13d36a6cfbd390cd5f3bf67452b1add
Matt Ramotar <mramotar@dropbox.com>
2021-12-23 15:31:33 -0500

    Style log in

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,7 @@
-import { Box, Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, useTheme } from "@material-ui/core";
 import GoogleLogin, { GoogleLoginResponse, GoogleLoginResponseOffline } from "react-google-login";
+import Memeboxer from "../../assets/Memeboxer.png";
 import extractUsername from "../../helpers/extractUsername";
-import dropbox from "../../images/dropbox.png";
 import continueWithGoogle from "../../lib/continueWithGoogle";
 import { GOOGLE_CLIENT_ID } from "../../util/secrets";
 import styles from "./login.module.scss";
@@ -16,6 +16,7 @@ interface ProfileObj {
 }
 
 export default function Login(): JSX.Element {
+  const theme = useTheme();
   const handleLogin = async (data: GoogleLoginResponse | GoogleLoginResponseOffline) => {
     if ("code" in data) return;
 
@@ -30,15 +31,17 @@ export default function Login(): JSX.Element {
   };
 
   return (
-    <Grid className={styles.root}>
-      <Box className={styles.modal}>
-        <img src={dropbox} alt="component box" />
-        <Typography variant="h5" className={styles.text}>
-          Welcome!
+    <Grid container className={styles.root}>
+      <Grid container className={styles.modal}>
+        <Typography variant="h4" style={{ fontFamily: "Space Grotesk", fontWeight: "bold", color: theme.palette.background.paper }}>
+          memeboxer
         </Typography>
+        <img src={Memeboxer} alt="component box" />
 
-        <GoogleLogin clientId={GOOGLE_CLIENT_ID ?? ""} buttonText="SSO" onSuccess={handleLogin} onFailure={handleLogin} cookiePolicy={"single_host_origin"} theme="dark" className={styles.button} />
-      </Box>
+        <GoogleLogin clientId={GOOGLE_CLIENT_ID ?? ""} onSuccess={handleLogin} onFailure={handleLogin} cookiePolicy={"single_host_origin"} theme="dark" className={styles.button} icon={true}>
+          <Typography style={{ fontFamily: "Space Grotesk", fontWeight: "bold" }}>Log in</Typography>
+        </GoogleLogin>
+      </Grid>
     </Grid>
   );
 }
