75805b4dae2db0b67cf36540a4771ba7fbb4bbb4
Matt Ramotar <mramotar@dropbox.com>
2022-01-03 20:27:43 -0500

    Fix delete meme reaction chip

--- src/components/MemeDetail/MemeReactions.tsx
+++ src/components/MemeDetail/MemeReactions.tsx
@@ -7,60 +7,75 @@ import { ReactionIdToUserIdsMap, UserIdToMemeReactionIdsMap } from "../MemeFeedI
 import ReactionChip from "../MemeFeedItem/ReactionChip";
 
 interface Props {
-  meme: GodMeme;
+    meme: GodMeme;
 }
 
 interface MemeReactionInfo {
-  reactionIdToUserIds: [string, string[]][];
-  userIdToMemeReactionIdsMap: UserIdToMemeReactionIdsMap;
+    reactionIdToUserIds: [string, string[]][];
+    userIdToMemeReactionIdsMap: UserIdToMemeReactionIdsMap;
 }
 
 export default function MemeReactions(props: Props): JSX.Element | null {
-  const reactions = useSelector((state: RootState) => state.meme.reactions);
+    const reactions = useSelector((state: RootState) => state.meme.reactions);
 
-  const [reactionIdToUserIdsMap, setReactionIdToUserIdsMap] = useState<ReactionIdToUserIdsMap | null>(null);
-  const [userIdToMemeReactionIdsMap, setUserIdToMemeReactionIdsMap] = useState<UserIdToMemeReactionIdsMap | null>(null);
-  const [memeReactionInfo, setMemeReactionInfo] = useState<MemeReactionInfo | null>(null);
+    const [reactionIdToUserIdsMap, setReactionIdToUserIdsMap] = useState<ReactionIdToUserIdsMap | null>(null);
+    const [userIdToMemeReactionIdsMap, setUserIdToMemeReactionIdsMap] = useState<UserIdToMemeReactionIdsMap | null>(null);
+    const [memeReactionInfo, setMemeReactionInfo] = useState<MemeReactionInfo | null>(null);
 
-  useEffect(() => {
-    if (reactions) {
-      const reactionIdToUserIdsMap: ReactionIdToUserIdsMap = {};
-      const userIdToMemeReactionIdsMap: UserIdToMemeReactionIdsMap = {};
+    useEffect(() => {
+        if (reactions) {
+            const reactionIdToUserIdsMap: ReactionIdToUserIdsMap = {};
+            const userIdToMemeReactionIdsMap: UserIdToMemeReactionIdsMap = {};
 
-      for (const reaction of reactions) {
-        if (reactionIdToUserIdsMap[reaction.reactionId]) reactionIdToUserIdsMap[reaction.reactionId].push(reaction.userId);
-        else reactionIdToUserIdsMap[reaction.reactionId] = [reaction.userId];
+            for (const reaction of reactions) {
+                if (reactionIdToUserIdsMap[reaction.reactionId]) reactionIdToUserIdsMap[reaction.reactionId].push(reaction.userId);
+                else reactionIdToUserIdsMap[reaction.reactionId] = [reaction.userId];
 
-        if (userIdToMemeReactionIdsMap[reaction.userId]) userIdToMemeReactionIdsMap[reaction.userId].push(reaction.id);
-        else userIdToMemeReactionIdsMap[reaction.userId] = [reaction.id];
-      }
+                if (userIdToMemeReactionIdsMap[reaction.userId]) {
+                    if (userIdToMemeReactionIdsMap[reaction.userId][reaction.reactionId]) {
+                        userIdToMemeReactionIdsMap[reaction.userId][reaction.reactionId].push(reaction.id);
+                    } else {
+                        userIdToMemeReactionIdsMap[reaction.userId][reaction.reactionId] = [reaction.id];
+                    }
+                } else {
+                    userIdToMemeReactionIdsMap[reaction.userId] = {
+                        [reaction.reactionId]: [reaction.id],
+                    };
+                }
+            }
 
-      setReactionIdToUserIdsMap(reactionIdToUserIdsMap);
-      setUserIdToMemeReactionIdsMap(userIdToMemeReactionIdsMap);
-    }
-  }, [reactions]);
+            setReactionIdToUserIdsMap(reactionIdToUserIdsMap);
+            setUserIdToMemeReactionIdsMap(userIdToMemeReactionIdsMap);
+        }
+    }, [reactions]);
 
-  useEffect(() => {
-    const reactionIdToUserIds = reactionIdToUserIdsMap && Object.keys(reactionIdToUserIdsMap) ? Object.entries(reactionIdToUserIdsMap) : [];
-    const memeReactionInfo: MemeReactionInfo = {
-      reactionIdToUserIds,
-      userIdToMemeReactionIdsMap: userIdToMemeReactionIdsMap ?? {},
-    };
+    useEffect(() => {
+        const reactionIdToUserIds = reactionIdToUserIdsMap && Object.keys(reactionIdToUserIdsMap) ? Object.entries(reactionIdToUserIdsMap) : [];
+        const memeReactionInfo: MemeReactionInfo = {
+            reactionIdToUserIds,
+            userIdToMemeReactionIdsMap: userIdToMemeReactionIdsMap ?? {},
+        };
 
-    setMemeReactionInfo(memeReactionInfo);
-  }, [reactionIdToUserIdsMap, userIdToMemeReactionIdsMap]);
+        setMemeReactionInfo(memeReactionInfo);
+    }, [reactionIdToUserIdsMap, userIdToMemeReactionIdsMap]);
 
-  if (!memeReactionInfo) return null;
+    if (!memeReactionInfo) return null;
 
-  return (
-    <Grid container>
-      <Grid container spacing={1} style={{ display: "flex", flexDirection: "row", width: "100%", flexWrap: "wrap" }}>
-        {memeReactionInfo.reactionIdToUserIds.map(([reactionId, userIds]) => (
-          <Grid item xs={2} key={reactionId} style={{}}>
-            <ReactionChip reactionId={reactionId} count={userIds.length} userIds={userIds} memeId={props.meme.id} userIdToMemeReactionIdsMap={memeReactionInfo.userIdToMemeReactionIdsMap} />
-          </Grid>
-        ))}
-      </Grid>
-    </Grid>
-  );
+    return (
+        <Grid container>
+            <Grid container spacing={1} style={{ display: "flex", flexDirection: "row", width: "100%", flexWrap: "wrap" }}>
+                {memeReactionInfo.reactionIdToUserIds.map(([reactionId, userIds]) => (
+                    <Grid item xs={2} key={reactionId} style={{}}>
+                        <ReactionChip
+                            reactionId={reactionId}
+                            count={userIds.length}
+                            userIds={userIds}
+                            memeId={props.meme.id}
+                            userIdToMemeReactionIdsMap={memeReactionInfo.userIdToMemeReactionIdsMap}
+                        />
+                    </Grid>
+                ))}
+            </Grid>
+        </Grid>
+    );
 }
--- src/components/MemeFeedItem/ReactionChip.tsx
+++ src/components/MemeFeedItem/ReactionChip.tsx
@@ -1,97 +1,114 @@
 import { Box, Typography, useTheme } from "@material-ui/core";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
-import { Reaction } from "../../types";
+import { setReactions } from "../../store/meme";
+import { GodMeme, Reaction } from "../../types";
 import { API_URL } from "../../util/secrets";
 import { UserIdToMemeReactionIdsMap } from "./MemeFeedItem";
 
 interface Props {
-  reactionId: string;
-  count: number;
-  userIds: string[];
-  memeId: string;
-  userIdToMemeReactionIdsMap: UserIdToMemeReactionIdsMap;
+    reactionId: string;
+    count: number;
+    userIds: string[];
+    memeId: string;
+    userIdToMemeReactionIdsMap: UserIdToMemeReactionIdsMap;
 }
 
 export default function ReactionChip(props: Props): JSX.Element | null {
-  const user = useSelector((state: RootState) => state.user);
-  const theme = useTheme();
-  const [reaction, setReaction] = useState<Reaction | null>(null);
-  const [count, setCount] = useState(props.count);
+    const theme = useTheme();
+    const dispatch = useDispatch();
 
-  const [userHasReacted, setUserHasReacted] = useState<boolean | null>(null);
+    const user = useSelector((state: RootState) => state.user);
 
-  useEffect(() => {
-    async function fetchReaction() {
-      const response = await axios.get(`${API_URL}/v1/reactions/${props.reactionId}`);
-      console.log("fetched reaction", response.data);
-      setReaction(response.data);
-    }
-    fetchReaction();
-  }, [props.reactionId]);
+    const [reaction, setReaction] = useState<Reaction | null>(null);
+    const [count, setCount] = useState(props.count);
+    const [userHasReacted, setUserHasReacted] = useState<boolean | null>(null);
+    const [userMemeReactionIds, setUserMemeReactionIds] = useState<string[] | null>(null);
 
-  useEffect(() => {
-    if (user && user.id) {
-      setUserHasReacted(Boolean(props.userIds.includes(user.id)));
-    } else {
-      setUserHasReacted(false);
-    }
-  }, [user]);
+    const onClick = () => {
+        async function deleteMemeReactionAsync(memeReactionIds: string[]) {
+            for (const memeReactionId of memeReactionIds) {
+                const response = await axios.delete(`${API_URL}/v1/memes/${props.memeId}/reactions/${memeReactionId}`, { data: { userId: user.id, token: user.token } });
+                const godMeme: GodMeme = response.data;
 
-  useEffect(() => {
-    setCount(props.count);
-  }, [props.count]);
+                if (godMeme.reactions) {
+                    dispatch(setReactions(godMeme.reactions));
+                }
+                setCount(count - 1);
+                setUserHasReacted(false);
+            }
+        }
 
-  const onClick = () => {
-    async function deleteMemeReactionAsync(memeReactionId: string) {
-      const response = await axios.delete(`${API_URL}/v1/memes/${props.memeId}/reactions/${memeReactionId}`, { data: { userId: user.id, token: user.token } });
-      setCount(count - 1);
-      setUserHasReacted(false);
-    }
+        async function createMemeReactionAsync() {
+            const addMemeReactionInput = {
+                reactionId: props.reactionId,
+                userId: user.id,
+            };
 
-    async function createMemeReactionAsync() {
-      const addMemeReactionInput = {
-        reactionId: props.reactionId,
-        userId: user.id,
-      };
+            const response = await axios.post(`${API_URL}/v1/memes/${props.memeId}/reactions`, addMemeReactionInput);
+            setCount(count + 1);
+            setUserHasReacted(true);
+        }
 
-      const response = await axios.post(`${API_URL}/v1/memes/${props.memeId}/reactions`, addMemeReactionInput);
-      setCount(count + 1);
-      setUserHasReacted(true);
-    }
+        if (user.id && userHasReacted == true && userMemeReactionIds) {
+            deleteMemeReactionAsync(userMemeReactionIds);
+        } else if (user.id && userHasReacted == false) {
+            createMemeReactionAsync();
+        }
+    };
 
-    if (user.id && userHasReacted == true) {
-      const memeReactionIds = props.userIdToMemeReactionIdsMap[user.id];
-      for (const memeReactionId of memeReactionIds) {
-        deleteMemeReactionAsync(memeReactionId);
-      }
-    } else if (user.id && userHasReacted == false) {
-      createMemeReactionAsync();
-    }
-  };
+    useEffect(() => {
+        async function fetchReaction() {
+            const response = await axios.get(`${API_URL}/v1/reactions/${props.reactionId}`);
+            console.log("fetched reaction", response.data);
+            setReaction(response.data);
+        }
+        fetchReaction();
+    }, [props.reactionId]);
 
-  if (!reaction) return null;
+    useEffect(() => {
+        if (user && user.id) {
+            setUserHasReacted(Boolean(props.userIds.includes(user.id)));
+        } else {
+            setUserHasReacted(false);
+        }
+    }, [user]);
 
-  return (
-    <Box
-      key={reaction.id}
-      style={{
-        display: count > 0 ? "flex" : "none",
-        flexDirection: "row",
-        backgroundColor: userHasReacted ? "#E3F2FD" : "#f1f1f1",
-        padding: "2px 6px",
-        border: userHasReacted ? `1px solid ${theme.palette.primary.main}` : "none",
-        borderRadius: 16,
-        cursor: "pointer",
-      }}
-      onClick={onClick}
-    >
-      <Typography variant="body2">{reaction.native}</Typography>
-      <Typography variant="body2" style={{ marginLeft: 4, color: userHasReacted ? theme.palette.primary.main : theme.palette.text.primary, fontFamily: "Space Grotesk" }}>
-        {count}
-      </Typography>
-    </Box>
-  );
+    useEffect(() => {
+        if (user && user.id) {
+            if (props.userIdToMemeReactionIdsMap[user.id] && props.userIdToMemeReactionIdsMap[user.id][props.reactionId]) {
+                setUserMemeReactionIds(props.userIdToMemeReactionIdsMap[user.id][props.reactionId]);
+            }
+        }
+    }, [user, props.userIdToMemeReactionIdsMap]);
+
+    useEffect(() => {
+        setCount(props.count);
+    }, [props.count]);
+
+    if (!reaction) return null;
+
+    return (
+        <Box
+            key={reaction.id}
+            style={{
+                display: count > 0 ? "flex" : "none",
+                visibility: count > 0 ? "visible" : "hidden",
+                flexDirection: "row",
+                backgroundColor: userHasReacted ? "#E3F2FD" : "#f1f1f1",
+                padding: "2px 6px",
+                border: userHasReacted ? `1px solid ${theme.palette.primary.main}` : "none",
+                borderRadius: 16,
+                cursor: "pointer",
+            }}
+            onClick={onClick}
+        >
+            <Typography variant="body2">{reaction.native}</Typography>
+            <Typography variant="body2" style={{ marginLeft: 4, color: userHasReacted ? theme.palette.primary.main : theme.palette.text.primary, fontFamily: "Space Grotesk" }}>
+                {count}
+            </Typography>
+        </Box>
+    );
 }
